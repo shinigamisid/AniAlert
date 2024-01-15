8e16f49c5024f452a45a2a6e592717f0db4f49fe
shini <tiruvalluri@gmail.com>
2024-01-15 17:53:52 +0530

    changed the program structure a little
    ani_alert.py has been renamed to main.py
    notification.py has been removed and its contents have been added to main.py

--- notification.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-def send_notification(title, message):
-    applescript_command = f'display notification "{message}" with title "{title}"'
-    subprocess.run(["osascript", "-e", applescript_command])
